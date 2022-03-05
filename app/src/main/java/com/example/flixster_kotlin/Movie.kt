e5f16d282eb0f1ad04a9f47594379cf56f77ca73
dingyufish <dingyuwang0102@gmail.com>
2022-03-05 01:30:49 -0500

    assiment 2

--- app/src/main/java/com/example/flixster_kotlin/Movie.kt
+++ app/src/main/java/com/example/flixster_kotlin/Movie.kt
@@ -1,15 +1,24 @@
 package com.example.flixster_kotlin
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.IgnoredOnParcel
 import org.json.JSONArray
+
+@Parcelize
 
 //repersent one movie abject to define the data
 data class Movie (
     val movieId: Int,
+    val voteAverage: Double,
     private val posterPath: String,
     val title: String,
-    val overView: String,
-        ){
+    val overView: String, ):Parcelable
+{
+    @IgnoredOnParcel
     val posterImageUrl = "https://image.tmdb.org/t/p/w342/$posterPath"
+
+
     companion object{
 
          //call methoe on the the moive class
@@ -20,6 +29,7 @@ data class Movie (
                 movies.add(
                     Movie(
                         movieJson.getInt("id"),
+                        movieJson.getDouble("vote_average"),
                         movieJson.getString("poster_path"),
                         movieJson.getString("title"),
                         movieJson.getString("overview")
